24633ec2fd412c4a342e29dff2f109e821d31416
ZhouZhou1235 <1479499289@qq.com>
2025-08-14 21:59:16 +0800

    【合并】保存冲突

--- bot/main.py
+++ bot/main.py
@@ -3,7 +3,7 @@
 from core.config_manager import config_manager
 from core.bot_launcher import create_bot
 
-# TODO 添加测试
+
 if __name__=='__main__':
     bot = create_bot()
     bot.run(bt_uin=config_manager.bot_config.qq_number)
